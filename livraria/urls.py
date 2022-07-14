538c6f031f167dc173a57e635eefc57a2fa8ee11
GuinneverC <guinnevercorrea.1@gmail.com>
2022-07-14 15:01:17 -0300

    commit 14-07

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
+router.register(r'editora', EditoraViewSet)
+router.register(r'autor', AutorViewSet)
+router.register(r'livro', LivroViewSet)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
