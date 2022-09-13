c673ceb4b286a38785c0d7b95d22816a5463a97c
GuinneverC <guinnevercorrea.1@gmail.com>
2022-09-13 16:14:04 -0300

    models organizadas

---
+++ core/models/livro.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .autor import Autor
+from .categoria import Categoria
+from .editora import Editora
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32)
+    quantidade = models.IntegerField
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="editora")
+    autores = models.ManyToManyField(Autor, related_name="livros")
+    
+    def __str__(self):
+        return self.titulo
